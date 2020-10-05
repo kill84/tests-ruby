07de9ed817f4b173738e16d90f4223782e46dc00
kill84 <71645149+kill84@users.noreply.github.com>
2020-10-05 16:08:43 +0200

    add alternative code to pig latin

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -9,3 +9,18 @@ def translate (sent)
         str  = ' ' + str + 'ay'
     end.strip
 end
+
+# def translate(a)
+#   split_words = Array.new
+#   words = a.split(' ')
+#
+#   words.each do |word|
+#     while word.start_with?('b','c','h','s','q','u','t','r','f','p')
+#       word = word.split('').rotate.join('')
+#     end
+#     word << "ay"
+#     split_words << word
+#   end
+#
+#   return split_words.join(' ')
+# end
